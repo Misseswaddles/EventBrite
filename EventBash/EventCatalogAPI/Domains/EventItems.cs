cf044454dde89c329f848eabf73fcf0bec504504
misseswaddles <carol.farris2@gmail.com>
2019-07-02 10:45:51 -0700

    EventCatalog API updating

--- EventBash/EventCatalogAPI/Domains/EventItems.cs
+++ EventBash/EventCatalogAPI/Domains/EventItems.cs
@@ -13,11 +13,10 @@ namespace EventCatalogAPI.Domains
         //This is tied to  and EventCategory (like Catalog Type
         public int EventCategoryId { get; set; } //foregin key
 
-        public virtual
+
 
         //this is tied to EventVenue
-        public int EventVenueId
-        { get; set; } //foreign key
+        public int EventVenueId { get; set; } //foreign key
 
     }
 }
