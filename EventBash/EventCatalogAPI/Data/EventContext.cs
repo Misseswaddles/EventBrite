4e8b08bf4770d16e739ab8bb03ea37be356cf384
misseswaddles <carol.farris2@gmail.com>
2019-07-02 11:39:16 -0700

    Updating EventSeed.cs and EventContext.cs

---
+++ EventBash/EventCatalogAPI/Data/EventContext.cs
@@ -0,0 +1,24 @@
+﻿using EventCatalogAPI.Domains;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public class EventContext : DbContext 
+    {
+        /* this will be added after we update the config file, and will make the clas injectable
+        public EventContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        */
+
+        public DbSet<EventCategory> EventCategories { get; set; }
+
+
+
+    }
+}
---
+++ EventBash/EventCatalogAPI/Data/EventSeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventCatalogAPI.Data
+{
+    public static class EventSeed
+    {
+        public static void Seed(EventContext context)
+        {
+            //context.Database.Migrate(); //Not yet!
+
+            //if()
+
+        }
+    }
+}
