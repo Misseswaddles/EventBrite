05269e095797c46f8c20bea54897d0910c5f57d3
misseswaddles <carol.farris2@gmail.com>
2019-07-02 14:13:52 -0700

    Updating

--- EventBash/EventCatalogAPI/Data/EventContext.cs
+++ EventBash/EventCatalogAPI/Data/EventContext.cs
@@ -30,14 +30,46 @@ namespace EventCatalogAPI.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder )
 
         //The EventSeed file will create a method to get 
-        //GetPreConfiguredEventItems, etc.
+        //ex. ConfigureEventVenue, etc.
         {
-            modelBuilder.Entity<EventVenue>(ConfigureEventVenue)
+            modelBuilder.Entity<EventVenue>(ConfigureEventVenue);
+            modelBuilder.Entity<EventCategory>(ConfigureEventCategory);
+            modelBuilder.Entity<EventItems>(ConfigureEventItems);
         }
 
-        private void ConfigureEventVenue(EntityTypeBuilder<EventVenue> obj)
+        private void ConfigureEventItems(EntityTypeBuilder<EventItems> builder)
         {
-            throw new NotImplementedException();
+            builder.ToTable("EventCatalog");//the actual name of the event catalog
+            builder.Property(c => c.Id)
+            
         }
+
+        private void ConfigureEventCategory(EntityTypeBuilder<EventCategory> builder)
+        {
+            builder.ToTable("EventCategories");
+
+            builder.Property(c => c.Id)
+                .IsRequired() //primary key
+                .ForSqlServerUseSequenceHiLo("event_category_hilo"); //
+
+            builder.Property(c => c.Type) //would be good to change type member to category.
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+
+        private void ConfigureEventVenue(EntityTypeBuilder<EventVenue> builder)
+        {
+            builder.ToTable("EventVenues");
+
+            builder.Property(c => c.Id) //What to do with the properties
+                .IsRequired() //required because Id is the primary key
+                .ForSqlServerUseSequenceHiLo("event_venue_hilo");
+
+            builder.Property(c => c.Venue)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+
+
     }
 }
