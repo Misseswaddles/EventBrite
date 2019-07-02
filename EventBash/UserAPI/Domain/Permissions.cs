cdf493c643a881eb63d7f072c627e7de168dc39c
rsrirekha <rsrirekha@outlook.com>
2019-07-02 12:10:12 -0700

    Adding permissions file to the UserAPi/Domain

---
+++ EventBash/UserAPI/Domain/Permissions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Permissions
+{
+  
+
+        public  int Id { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+}
