8a4e4a43274c7e33f7d1da6c341b90fadb3bf0cc
DivyaKumari1 <37888285+DivyaKumari1@users.noreply.github.com>
2019-07-04 22:48:18 -0700

    Added User controller

--- EventBash/UserAPI/Controllers/UserController.cs
+++ EventBash/UserAPI/Controllers/UserController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserAPI.Data;
 
 namespace UserAPI.Controllers
 {
@@ -11,7 +14,27 @@ namespace UserAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        public void test() { }
+        private readonly UserContext _context;
+        public UserController(UserContext context)
+        {
+            _context = context;
+        }
 
+        //GET api/userinformation/users?pageSize=10&pageIndex=2
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Users([FromQuery]int pageSize = 5,[FromQuery]int pageIndex =0)
+        {
+            var usersCount = await _context.UserInformation.LongCountAsync();
+
+            var users = await _context.UserInformation
+                  .OrderBy(c => c.FirstName)
+                  .Skip(pageSize * pageIndex)
+                  .Take(pageSize)
+                  .ToListAsync();
+            return Ok(users);
+
+        }
+ 
     }
 }
