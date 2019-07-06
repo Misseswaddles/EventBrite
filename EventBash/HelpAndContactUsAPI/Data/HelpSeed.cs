a08d02bcbb4fb12529ac0e1ec42730497d3ef8ea
DivyaKumari1 <37888285+DivyaKumari1@users.noreply.github.com>
2019-07-06 07:21:50 -0700

    User controller, HelpandContactUS API without seed data...

---
+++ EventBash/HelpAndContactUsAPI/Data/HelpSeed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelpAndContactUsAPI.Domain;
+
+namespace HelpAndContactUsAPI.Data
+{
+    public class HelpSeed
+    {
+        public static void Seed(HelpContext context)
+        {
+            if (!context.Helps.Any())
+            {
+                context.Helps
+                    .AddRange(GetPreConfiguredHelps());
+
+                context.SaveChanges();
+            }
+        }
+
+        private static IEnumerable<Help> GetPreConfiguredHelps()
+        {
+            return new List<Help>()
+            {
+            };
+
+           
+        }
+    }
+}
